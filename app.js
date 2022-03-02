280cbdc33c42a61bb3850bb52e3dae27ed4fc5a7
wells <46527325+wellspeter@users.noreply.github.com>
2022-03-02 14:25:18 -0300

    ao entrar carrega ja o app

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Rotas
 app.get('/', function(req, res){
-    res.send("serfico foi!")
+    res.render('cadastrocliente');
 });
 
 app.get('/cadastrocliente', function(req, res){
